40d7611b2806254874bb047eb63eb0dd17858b92
Sidhu503 <113715678+Sidhu503@users.noreply.github.com>
2023-05-06 11:24:11 +0530

    Update demofile.java

--- demofile.java
+++ demofile.java
@@ -1,3 +1,5 @@
-public class demofile{
-System.out.print("This is first build job in jenkins!!!!!");
+class demofile{
+  public static void main(String args[]){
+        System.out.print("This is first build job in jenkins!!!!!");
+  }
 }
